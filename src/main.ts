5ef80614f818bb1bbc3c7c095e601ae71bafaed3
Artem Pivovarov <pivovarov.artyom1@gmail.com>
2023-08-26 14:39:23 +0700

    chore(main): set ignore trailing slash

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ import {
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter()
+    new FastifyAdapter({ ignoreTrailingSlash: true })
   )
 
   app.setGlobalPrefix('api')
