47d76ab30c6823170a13ee96e6f8bae405e511e7
Artem Pivovarov <pivovarov.artyom1@gmail.com>
2023-08-26 10:16:51 +0700

    chore(platform): migrate to fastify

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import 'module-alias/register'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
+import {
+  FastifyAdapter,
+  NestFastifyApplication
+} from '@nestjs/platform-fastify'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  )
 
   app.setGlobalPrefix('api')
   app.enableCors()
