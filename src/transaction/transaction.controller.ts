6f3e4eabde6c7b329f983ad1a49bc46e8d48c588
Artem Pivovarov <pivovarov.artyom1@gmail.com>
2023-08-27 10:41:42 +0700

    feat(transaction): implement read, update and delete

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,10 +1,22 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common'
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Get,
+  Param,
+  Delete,
+  Query,
+  Patch
+} from '@nestjs/common'
 import { TransactionService } from './transaction.service'
 import { CreateTransactionDto } from './dto/create-transaction.dto'
 import { JwtAuthGuard } from '@/auth/jwt-auth.guard'
 import { Roles } from '@/role/roles.decorator'
 import { Role } from '@/role/role.enum'
 import { RolesGuard } from '@/role/roles.guard'
+import { PaginationQueryDto } from '@/common/dto/pagination-query.dto'
+import { UpdateTransactionDto } from '@/transaction/dto/update-transactions.dto'
 
 @Controller('transactions')
 export class TransactionController {
@@ -15,5 +27,62 @@ export class TransactionController {
   @UseGuards(JwtAuthGuard, RolesGuard)
   create(@Body() createTransactionDto: CreateTransactionDto) {
     return this.transactionService.create(createTransactionDto)
+  }
+
+  @Get()
+  @Roles(Role.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  async findAll(@Query() paginationQueryDto: PaginationQueryDto) {
+    return this.transactionService.findAll(paginationQueryDto)
+  }
+
+  @Get(':id')
+  @Roles(Role.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  async findOne(@Param('id') id: number) {
+    return this.transactionService.findOne(+id)
+  }
+
+  @Get('user/:userId')
+  @Roles(Role.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  async findByUser(
+    @Param('userId') userId: number,
+    @Query() paginationQueryDto: PaginationQueryDto
+  ) {
+    return this.transactionService.findUserTransactions(
+      +userId,
+      paginationQueryDto
+    )
+  }
+
+  @Get('product/:productId')
+  @Roles(Role.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  async findByProduct(
+    @Param('productId') productId: number,
+    @Query() paginationQueryDto: PaginationQueryDto
+  ) {
+    return this.transactionService.findProductTransactions(
+      +productId,
+      paginationQueryDto
+    )
+  }
+
+  @Patch(':id')
+  @Roles(Role.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  async update(
+    @Param('id') id: number,
+    @Body() updateTransactionDto: UpdateTransactionDto
+  ) {
+    await this.transactionService.update(+id, updateTransactionDto)
+  }
+
+  @Delete(':id')
+  @Roles(Role.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  async delete(@Param('id') id: number) {
+    await this.transactionService.delete(+id)
   }
 }
--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -5,6 +5,9 @@ import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { User } from '@/user/entities/user.entity'
 import { Product } from '@/product/entities/product.entity'
+import { PaginatedResponse } from '@/common/types/pagination.types'
+import { PaginationQueryDto } from '@/common/dto/pagination-query.dto'
+import { UpdateTransactionDto } from '@/transaction/dto/update-transactions.dto'
 
 @Injectable()
 export class TransactionService {
@@ -42,5 +45,216 @@ export class TransactionService {
       user: { id: userId },
       product: { id: productId }
     })
+  }
+
+  async findAll(
+    paginationQueryDto: PaginationQueryDto
+  ): Promise<PaginatedResponse<Transaction>> {
+    const { page, limit } = paginationQueryDto
+    const [items, totalItems] = await this.transactionRepository.findAndCount({
+      select: {
+        id: true,
+        createdAt: true,
+        user: {
+          id: true,
+          email: true,
+          name: true
+        },
+        product: {
+          id: true,
+          slug: true,
+          nameEn: true,
+          nameRu: true,
+          priceUsd: true,
+          priceRub: true,
+          priceBtc: true
+        }
+      },
+      relations: {
+        user: true,
+        product: true
+      },
+      take: limit,
+      skip: (page - 1) * limit
+    })
+
+    return {
+      items,
+      meta: {
+        page,
+        limit,
+        totalItems,
+        totalPages: Math.ceil(totalItems / limit)
+      }
+    }
+  }
+
+  async findOne(id: Transaction['id']) {
+    const transaction = await this.transactionRepository.findOne({
+      select: {
+        id: true,
+        createdAt: true,
+        user: {
+          id: true,
+          email: true,
+          name: true
+        },
+        product: {
+          id: true,
+          slug: true,
+          nameEn: true,
+          nameRu: true,
+          priceUsd: true,
+          priceRub: true,
+          priceBtc: true
+        }
+      },
+      where: {
+        id
+      },
+      relations: {
+        user: true,
+        product: true
+      }
+    })
+    if (!transaction) {
+      throw new BadRequestException('Transaction not found')
+    }
+
+    return transaction
+  }
+
+  async findUserTransactions(
+    userId: User['id'],
+    paginationQueryDto: PaginationQueryDto
+  ): Promise<PaginatedResponse<Transaction>> {
+    const { page, limit } = paginationQueryDto
+    const [items, totalItems] = await this.transactionRepository.findAndCount({
+      select: {
+        id: true,
+        createdAt: true,
+        product: {
+          id: true,
+          slug: true,
+          nameEn: true,
+          nameRu: true,
+          priceUsd: true,
+          priceRub: true,
+          priceBtc: true
+        }
+      },
+      where: {
+        user: { id: userId }
+      },
+      relations: {
+        product: true
+      },
+      take: limit,
+      skip: (page - 1) * limit
+    })
+
+    return {
+      items,
+      meta: {
+        page,
+        limit,
+        totalItems,
+        totalPages: Math.ceil(totalItems / limit)
+      }
+    }
+  }
+
+  async findProductTransactions(
+    productId: Product['id'],
+    paginationQueryDto: PaginationQueryDto
+  ): Promise<PaginatedResponse<Transaction>> {
+    const { page, limit } = paginationQueryDto
+    const [items, totalItems] = await this.transactionRepository.findAndCount({
+      select: {
+        id: true,
+        createdAt: true,
+        user: {
+          id: true,
+          email: true,
+          name: true
+        }
+      },
+      where: {
+        product: { id: productId }
+      },
+      relations: {
+        user: true
+      },
+      take: limit,
+      skip: (page - 1) * limit
+    })
+
+    return {
+      items,
+      meta: {
+        page,
+        limit,
+        totalItems,
+        totalPages: Math.ceil(totalItems / limit)
+      }
+    }
+  }
+
+  async update(
+    id: Transaction['id'],
+    updateTransactionDto: UpdateTransactionDto
+  ) {
+    const transaction = await this.transactionRepository.findOne({
+      where: {
+        id
+      }
+    })
+    if (!transaction) {
+      throw new BadRequestException('Transaction not found')
+    }
+
+    const { productId, userId, ...rest } = updateTransactionDto
+    const transactionUpdatePayload = { ...rest }
+
+    if (productId) {
+      const product = await this.productRepository.findOne({
+        where: {
+          id: productId
+        }
+      })
+      if (!product) {
+        throw new BadRequestException('Product not found')
+      }
+
+      transactionUpdatePayload['product'] = { id: productId }
+    }
+
+    if (userId) {
+      const user = await this.userRepository.findOne({
+        where: {
+          id: userId
+        }
+      })
+      if (!user) {
+        throw new BadRequestException('User not found')
+      }
+
+      transactionUpdatePayload['user'] = { id: userId }
+    }
+
+    return this.transactionRepository.update(id, transactionUpdatePayload)
+  }
+
+  async delete(id: Transaction['id']) {
+    const transaction = await this.transactionRepository.findOne({
+      where: {
+        id
+      }
+    })
+    if (!transaction) {
+      throw new BadRequestException('Transaction not found')
+    }
+
+    return this.transactionRepository.delete(id)
   }
 }
