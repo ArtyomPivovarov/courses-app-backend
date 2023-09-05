2024dfd515f395424b5352cd67ecd8fa1ba58b7b
Artem Pivovarov <pivovarov.artyom1@gmail.com>
2023-09-05 08:11:51 +0700

    chore(product): improve swagger doc

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -16,11 +16,15 @@ import { JwtAuthGuard } from '@/auth/jwt-auth.guard'
 import { Role } from '@/role/role.enum'
 import { Roles } from '@/role/roles.decorator'
 import { RolesGuard } from '@/role/roles.guard'
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger'
 
+@ApiTags('products')
 @Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
+  @ApiOperation({ summary: 'Create product' })
+  @ApiBearerAuth()
   @Post()
   @Roles(Role.Admin)
   @UseGuards(JwtAuthGuard, RolesGuard)
@@ -28,16 +32,20 @@ export class ProductController {
     return this.productService.create(createProductDto)
   }
 
+  @ApiOperation({ summary: 'Retrieve all products' })
   @Get()
   findAll(@Query() paginationQueryDto: PaginationQueryDto) {
     return this.productService.findAll(paginationQueryDto)
   }
 
+  @ApiOperation({ summary: 'Retrieve a product by slug' })
   @Get(':slug')
   findOne(@Param('slug') slug: string) {
     return this.productService.findOne(slug)
   }
 
+  @ApiOperation({ summary: 'Update product by ID' })
+  @ApiBearerAuth()
   @Patch(':id')
   @Roles(Role.Admin)
   @UseGuards(JwtAuthGuard, RolesGuard)
